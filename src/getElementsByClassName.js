3cfcb9f74f8088604c499c099d9ca5d4007ea0b7
Zach Gibb <zachgibb@gmail.com>
2014-07-15 18:31:19 -0400

    Fixed that a lot faster than i thought!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,15 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  var elementArray = [];
+  var elements = document.body.getElementsByTagName("*");
+  if (document.body.classList.contains(className)) {
+    elementArray.push(document.body);
+  };
+  for (var i = 0; i != elements.length; i++) {
+    if (elements[i].classList.contains(className)) {
+      elementArray.push(elements[i]);
+    }
+  };
+  return elementArray;
 };
