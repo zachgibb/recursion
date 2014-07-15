582b7d578bfe64f21a8ff23cb51fd824bd119624
Zach Gibb <zachgibb@gmail.com>
2014-07-15 09:16:08 -0400

    Added switch statement to deal with different data types. Still working on objects and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,31 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+  switch(typeof obj) {
+      case "undefined":
+          return undefined;
+
+      case "boolean":
+          // fall through
+      case "number":
+          // fall through
+      case "function":
+          return obj.toString();
+
+      case "string":
+          obj = "\"" + obj + "\"";
+          return obj;
+
+      case "object":
+          if (obj == null) {
+            return 'null';
+          } else if (Array.isArray(obj)) {
+
+            obj = "["+ obj +"]";
+            return obj;
+          } else {
+            console.log('this is a real object');
+          }
+  }
 };
