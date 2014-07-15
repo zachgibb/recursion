5a642b93b7b34ebcc23f610f912535286a70a228
Zach Gibb <zachgibb@gmail.com>
2014-07-15 13:23:00 -0400

    Fixed array and object iterators. Trying to figure out if this is actually working.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,6 +5,8 @@
 var stringifyJSON = function(obj) {
   console.log(obj);
   switch(typeof obj) {
+      case "function":
+          // fall through
       case "undefined":
           return undefined;
 
@@ -12,7 +14,6 @@ var stringifyJSON = function(obj) {
           // fall through
       case "number":
           // fall through
-      case "function":
           return obj.toString();
 
       case "string":
@@ -24,10 +25,45 @@ var stringifyJSON = function(obj) {
             return 'null';
           } else if (Array.isArray(obj)) {
 
-            obj = "["+ obj +"]";
+            var newObj = []; 
+            for (var i = 0; i != obj.length; i++) {
+              
+              var item = stringifyJSON(obj[i]);
+              newObj.push(item);
+            };
+            obj = "["+ newObj.join() +"]";
             return obj;
+
           } else {
+
             console.log('this is a real object');
+            var newObj = '';
+            var i = 0; 
+
+            for (var prop in obj) {
+
+              var key = stringifyJSON(prop) + ":";
+              var value = stringifyJSON(obj[prop]);
+
+              if (value !== undefined) {
+
+                if(i != 0) {
+
+                  newObj += ",";
+
+                }
+
+                i++;
+                newObj += (key + value);
+
+              }
+              
+            };
+            obj = "{"+ newObj +"}";
+
+            console.log(obj);
+            return obj;
+
           }
   }
 };
